eb48dc2d23fdc10e4e0949db055bfcb300fc92f0
Jules Bertholet <jules.bertholet@gmail.com>
2021-05-25 09:28:16 -0400

    Update to 2021-05-31

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,22 +5,25 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="acl swig libpq-dev postgresql postgresql-contrib postgresql-common ffmpeg libimage-exiftool-perl curl libopenblas-dev libmagic1 libboost-all-dev libxrender-dev liblapack-dev git bzip2 cmake build-essential libsm6 libglib2.0-0 libgl1-mesa-glx gfortran gunicorn libheif-dev libssl-dev rustc liblzma-dev python3 python3-pip python3-venv imagemagick xsel nodejs npm redis-server libmagickwand-dev libldap2-dev libsasl2-dev ufraw-batch"
+pkg_dependencies="unzip ca-certificates swig libpq-dev postgresql postgresql-contrib postgresql-common ffmpeg libimage-exiftool-perl curl libopenblas-dev libmagic1 libboost-all-dev libxrender-dev liblapack-dev git bzip2 cmake build-essential libsm6 libglib2.0-0 libgl1-mesa-glx gfortran gunicorn libheif-dev libssl-dev rustc liblzma-dev python3 python3-pip python3-venv imagemagick xsel nodejs npm redis-server libmagickwand-dev libldap2-dev libsasl2-dev ufraw-batch"
+
+arch="$(dpkg --print-architecture)"
 
 #=================================================
 # PERSONAL HELPERS
 #=================================================
 
 function unpack_source {
-	mkdir -p $final_path/data_models/{places365,im2txt}
+	ynh_secure_remove "$final_path"
+	mkdir -p "$final_path/data_models/"{places365,im2txt}
 	ynh_setup_source --source_id="places365_model" --dest_dir="$final_path/data_models/places365/model/"
 	ynh_setup_source --source_id="im2txt_model" --dest_dir="$final_path/data_models/im2txt/model/"
 	ynh_setup_source --source_id="im2txt_data" --dest_dir="$final_path/data_models/im2txt/data/"
-	mkdir -p $data_path
+	mkdir -p "$data_path"
 	ln -sf "$final_path/data_models" "$data_path/data_models"
-	mkdir -p $data_path/protected_media/{thumbnails_big,square_thumbnails,square_thumbnails_small,faces}
-	mkdir -p $data_path/data/nextcloud_media
-	mkdir -p $data_path/matplotlib
+	mkdir -p "$data_path/protected_media/"{thumbnails_big,square_thumbnails,square_thumbnails_small,faces}
+	mkdir -p "$data_path/data/nextcloud_media"
+	mkdir -p "$data_path/matplotlib"
 
 	mkdir -p ~/.cache/torch/hub/checkpoints/
 	ynh_setup_source --source_id="resnet152-b121ed2d" --dest_dir="/root/.cache/torch/hub/checkpoints/"
@@ -28,30 +31,72 @@ function unpack_source {
 	ynh_setup_source --source_id="backend" --dest_dir="$final_path/backend/"
 	ynh_setup_source --source_id="frontend" --dest_dir="$final_path/frontend/"
 	ynh_setup_source --source_id="linux" --dest_dir="$final_path/linux/"
+	ynh_setup_source --source_id="dlib" --dest_dir="$final_path/backend/dlib/"
+	if [ "$arch" = "aarch64" ]; then
+		export CONDA_DIR="$final_path/backend/conda"
+		mkdir -p "$CONDA_DIR"
+		ynh_setup_source --source_id="miniforge3" --dest_dir="$CONDA_DIR"
+		ynh_setup_source --source_id="faiss" --dest_dir="$final_path/backend/faiss/"
+	fi
 	mkdir -p "$final_path/bin"
 	mv -f "$final_path/linux/ressources/bin/"* "$final_path/bin"
 	ynh_secure_remove --file="$final_path/linux"
 
-	mkdir -p /var/log/$app
+	mkdir -p "/var/log/$app"
 }
 
 function set_up_virtualenv {
-	backend_path=$final_path/backend
-	pushd $backend_path || ynh_die
-		chown -R $app:$app $backend_path
+	backend_path="$final_path/backend"
+	pushd "$backend_path"
+		chown -R $app:$app "$backend_path"
 		sudo -u $app python3 -m venv $backend_path/venv
-		sudo -u $app $backend_path/venv/bin/pip --cache-dir $backend_path/.cache/pip install -U wheel pip setuptools 2>&1
-		sudo -u $app $backend_path/venv/bin/pip --cache-dir $backend_path/.cache/pip install -U torch==1.8.0+cpu torchvision==0.9.0+cpu -f https://download.pytorch.org/whl/torch_stable.html 2>&1 || sudo -u $app $backend_path/venv/bin/pip --cache-dir $backend_path/.cache/pip install -U torch==1.8.0 torchvision==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html 2>&1
-		sudo -u $app $backend_path/venv/bin/pip --cache-dir $backend_path/.cache/pip install -U --install-option="--no" --install-option="DLIB_USE_CUDA" --install-option="--no" --install-option="USE_AVX_INSTRUCTIONS" --install-option="--no" --install-option="USE_SSE4_INSTRUCTIONS" dlib 2>&1
-		sudo -u $app $backend_path/venv/bin/pip --cache-dir $backend_path/.cache/pip install -U --requirement $backend_path/requirements.txt 2>&1
-		sudo -u $app $backend_path/venv/bin/pip --cache-dir $backend_path/.cache/pip install -U --requirement $backend_path/requirements-ynh.txt 2>&1
-		sudo -u $app $backend_path/venv/bin/python -m spacy download en_core_web_sm 2>&1
-		chown -R root:root $backend_path
-	popd || ynh_die
+		local python_path="$backend_path/venv/bin:$(sudo -u $app bash -c 'echo $PATH')"
+		local cache_dir="$backend_path/.cache/pip"
+		sudo -u $app env "PATH=$python_path" pip --cache-dir "$cache_dir" install -U wheel pip setuptools 2>&1
+		if [ "$arch" = "aarch64" ]; then
+			sudo -u $app "$pip" --cache-dir "$cache_dir" install -U torch==1.8.0 torchvision==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html 2>&1 || sudo -u $app "$pip" --cache-dir "$cache_dir" install -U torch==1.8.0 torchvision==0.9.0 -f https://torch.maku.ml/whl/stable.html 2>&1
+			sudo -u $app env "CONDA_DIR=$CONDA_DIR" bash "${CONDA_DIR}/Miniforge3-4.10.1-4-Linux-aarch64.sh" -b -p "${CONDA_DIR}"
+			local conda="$CONDA_DIR/condabin/conda"
+			python_path="$CONDA_DIR/condabin:$CONDA_DIR/bin:$python_path"
+			sudo -u $app env "PATH=$python_path" conda install -y numpy psycopg2 cython pandas scikit-learn=0.24.1 scikit-image=0.18.1 spacy=2.3.5 gevent=20.12.1 matplotlib=3.3.2
+			pushd "$backend_path/faiss"
+				sudo -u $app cmake -B build .
+				sudo -u $app make -C build -j faiss
+				sudo -u $app make -C build -j swigfaiss
+				cd "build/faiss/python"
+				sudo -u $app env "PATH=$python_path" python setup.py install
+			popd
+			sed -i "/spacy==2.3.2/d" "$backend_path/requirements.txt"
+			sed -i "/sklearn==0.0/d" "$backend_path/requirements.txt"
+			sed -i "/gevent==20.9.0/d" "$backend_path/requirements.txt"
+			sed -i "/scipy==1.5.3/d" "$backend_path/requirements.txt"
+			sed -i "s/pytz==2020.1/pytz>=2021.1/" "$backend_path/requirements.txt"
+			sed -i "s/Pillow==8.1.0/Pillow>=8.1.2/" "$backend_path/requirements.txt"
+			sed -i "/faiss-cpu==1.7.0/d" "$backend_path/requirements.txt"
+		else
+			sudo -u $app env "PATH=$python_path" pip --cache-dir "$cache_dir" install -U torch==1.8.0+cpu torchvision==0.9.0+cpu -f https://download.pytorch.org/whl/torch_stable.html 2>&1 || sudo -u $app "$pip" --cache-dir "$cache_dir" install -U torch==1.8.0 torchvision==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html 2>&1
+		fi
+		pushd "$backend_path/dlib"
+			sudo -u $app env "PATH=$python_path" python setup.py install 2>&1
+		popd
+		sudo -u $app env "PATH=$python_path" pip --cache-dir "$cache_dir" install -U --requirement "$backend_path/requirements.txt" 2>&1
+		sudo -u $app env "PATH=$python_path" pip --cache-dir "$cache_dir" install -U --requirement "$backend_path/requirements-ynh.txt" 2>&1
+		sudo -u $app env "PATH=$python_path" python -m spacy download en_core_web_sm 2>&1
+		if [ "$arch" = "aarch64" ]; then
+			unzip "$CONDA_DIR/lib/python3.8/site-packages/"faiss*.egg -d "$CONDA_DIR/lib/python3.8/site-packages/"
+		fi
+		chown -R root:root "$backend_path"
+	popd
+}
+
+function install_dlib {
+	pushd "$backend_path/dlib"
+		sudo -u $app "$backend_path/venv/bin/python" setup.py install 2>&1
+	popd
 }
 
 function set_node_vars {
-	ynh_exec_warn_less ynh_install_nodejs --nodejs_version=10
+	ynh_exec_warn_less ynh_install_nodejs --nodejs_version=13
 	ynh_use_nodejs
 	node_PATH=$nodejs_path:$(sudo -u $app sh -c 'echo $PATH')
 
@@ -60,14 +105,14 @@ function set_node_vars {
 function set_up_frontend {
 	set_node_vars
 	frontend_path=$final_path/frontend
-	pushd $final_path/frontend || ynh_die
+	pushd $final_path/frontend
 		chown -R $app:$app $frontend_path
 		sudo -u $app touch $frontend_path/.yarnrc
 		sudo -u $app env "PATH=$node_PATH" yarn --cache-folder $frontend_path/yarn-cache --use-yarnrc $frontend_path/.yarnrc install 2>&1
 		sudo -u $app env "PATH=$node_PATH" yarn --cache-folder $frontend_path/yarn-cache --use-yarnrc $frontend_path/.yarnrc run build 2>&1
 		sudo -u $app env "PATH=$node_PATH" yarn --cache-folder $frontend_path/yarn-cache --use-yarnrc $frontend_path/.yarnrc add serve 2>&1
 		chown -R root:root $frontend_path
-	popd || ynh_die
+	popd
 }
 
 function add_configuations {
@@ -103,15 +148,15 @@ function add_configuations {
 }
 
 function set_permissions {
-	chown -R root:$app $final_path
-	chmod -R g=u,g-w,o-rwx $final_path
-	chown -R $app:$app $data_path
-	chmod -R g=u,g-w,o-rwx $data_path
-	chown -R $app:$app $final_path/data_models
-	chown -R $app:$app /var/log/$app
-	chmod -R g-w,o-rwx /var/log/$app
-	setfacl -n -m user:www-data:rx $data_path
-	setfacl -n -R -m user:www-data:rx -m default:user:www-data:rx $data_path/protected_media $data_path/data $data_path/data/nextcloud_media
+	chown -R root:$app "$final_path"
+	chmod -R g=u,g-w,o-rwx "$final_path"
+	chown -R $app:$app "$data_path"
+	chmod -R g=u,g-w,o-rwx "$data_path"
+	chown -R $app:$app "$final_path/data_models"
+	chown -R $app:$app "/var/log/$app"
+	chmod -R g-w,o-rwx "/var/log/$app"
+	setfacl -n -m user:www-data:rx "$data_path"
+	setfacl -nR -m u:www-data:rx -m d:u:www-data:rx "$data_path/protected_media" "$data_path/data" "$data_path/data/nextcloud_media"
 }
 
 function set_up_logrotate {

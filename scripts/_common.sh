e9e2814f6ce737d5c0f3cbb30ea3f3dde1745d86
Jules Bertholet <jules.bertholet@gmail.com>
2021-06-10 16:03:39 -0400

    Update to version 2020-06-09, add ARM support

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,9 +5,10 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="unzip ca-certificates swig libpq-dev postgresql postgresql-contrib postgresql-common ffmpeg libimage-exiftool-perl curl libopenblas-dev libmagic1 libboost-all-dev libxrender-dev liblapack-dev git bzip2 cmake build-essential libsm6 libglib2.0-0 libgl1-mesa-glx gfortran gunicorn libheif-dev libssl-dev rustc liblzma-dev python3 python3-pip python3-venv imagemagick xsel nodejs npm redis-server libmagickwand-dev libldap2-dev libsasl2-dev ufraw-batch"
+pkg_dependencies="libtinfo5 unzip ca-certificates swig libpq-dev postgresql postgresql-contrib postgresql-common ffmpeg libimage-exiftool-perl curl libopenblas-dev libmagic1 libboost-all-dev libxrender-dev liblapack-dev git bzip2 cmake build-essential libsm6 libglib2.0-0 libgl1-mesa-glx gfortran gunicorn libheif-dev libssl-dev rustc liblzma-dev python3 python3-pip python3-venv imagemagick xsel nodejs npm redis-server libmagickwand-dev libldap2-dev libsasl2-dev ufraw-batch"
 
 arch="$(dpkg --print-architecture)"
+arm64_test=1
 
 #=================================================
 # PERSONAL HELPERS
@@ -16,9 +17,8 @@ arch="$(dpkg --print-architecture)"
 function unpack_source {
 	ynh_secure_remove "$final_path"
 	mkdir -p "$final_path/data_models/"{places365,im2txt}
-	ynh_setup_source --source_id="places365_model" --dest_dir="$final_path/data_models/places365/model/"
-	ynh_setup_source --source_id="im2txt_model" --dest_dir="$final_path/data_models/im2txt/model/"
-	ynh_setup_source --source_id="im2txt_data" --dest_dir="$final_path/data_models/im2txt/data/"
+	ynh_setup_source --source_id="places365" --dest_dir="$final_path/data_models/places365/"
+	ynh_setup_source --source_id="im2txt" --dest_dir="$final_path/data_models/im2txt/"
 	mkdir -p "$data_path"
 	ln -sf "$final_path/data_models" "$data_path/data_models"
 	mkdir -p "$data_path/protected_media/"{thumbnails_big,square_thumbnails,square_thumbnails_small,faces}
@@ -30,39 +30,43 @@ function unpack_source {
 
 	ynh_setup_source --source_id="backend" --dest_dir="$final_path/backend/"
 	ynh_setup_source --source_id="frontend" --dest_dir="$final_path/frontend/"
-	ynh_setup_source --source_id="linux" --dest_dir="$final_path/linux/"
 	ynh_setup_source --source_id="dlib" --dest_dir="$final_path/backend/dlib/"
-	if [ "$arch" = "aarch64" ]; then
+	if [ "$arch" = "arm64" ] || [ "$arm64_test" -eq 1 ]; then
 		export CONDA_DIR="$final_path/backend/conda"
 		mkdir -p "$CONDA_DIR"
-		ynh_setup_source --source_id="miniforge3" --dest_dir="$CONDA_DIR"
-		ynh_setup_source --source_id="faiss" --dest_dir="$final_path/backend/faiss/"
+		if [ "$arch" = "arm64" ]; then
+			ynh_setup_source --source_id="miniforge3" --dest_dir="$CONDA_DIR"
+			ynh_setup_source --source_id="cmake" --dest_dir="$final_path/backend/cmake/"
+		else
+			wget -O "${CONDA_DIR}/Miniforge3-4.10.1-4-Linux-aarch64.sh" https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh 2>&1
+			ynh_setup_source --source_id="cmake_amd64" --dest_dir="$final_path/backend/cmake/"
+		fi
+			ynh_setup_source --source_id="faiss" --dest_dir="$final_path/backend/faiss/"
 	fi
-	mkdir -p "$final_path/bin"
-	mv -f "$final_path/linux/ressources/bin/"* "$final_path/bin"
-	ynh_secure_remove --file="$final_path/linux"
 
 	mkdir -p "/var/log/$app"
 }
 
-function set_up_virtualenv {
+function set_up_backend {
 	backend_path="$final_path/backend"
 	pushd "$backend_path"
 		chown -R $app:$app "$backend_path"
 		sudo -u $app python3 -m venv $backend_path/venv
-		local python_path="$backend_path/venv/bin:$(sudo -u $app bash -c 'echo $PATH')"
+		path_prefix="$backend_path/venv/bin"
+		if [ "$arch" = "arm64" ] || [ "$arm64_test" -eq 1 ]; then
+			path_prefix="$backend_path/cmake/bin:$CONDA_DIR/condabin:$CONDA_DIR/bin:$path_prefix"
+		fi
+		local python_path="$path_prefix:$(sudo -u $app bash -c 'echo $PATH')"
 		local cache_dir="$backend_path/.cache/pip"
 		sudo -u $app env "PATH=$python_path" pip --cache-dir "$cache_dir" install -U wheel pip setuptools 2>&1
-		if [ "$arch" = "aarch64" ]; then
-			sudo -u $app "$pip" --cache-dir "$cache_dir" install -U torch==1.8.0 torchvision==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html 2>&1 || sudo -u $app "$pip" --cache-dir "$cache_dir" install -U torch==1.8.0 torchvision==0.9.0 -f https://torch.maku.ml/whl/stable.html 2>&1
-			sudo -u $app env "CONDA_DIR=$CONDA_DIR" bash "${CONDA_DIR}/Miniforge3-4.10.1-4-Linux-aarch64.sh" -b -p "${CONDA_DIR}"
-			local conda="$CONDA_DIR/condabin/conda"
-			python_path="$CONDA_DIR/condabin:$CONDA_DIR/bin:$python_path"
+		if [ "$arch" = "arm64" ] || [ "$arm64_test" -eq 1 ]; then
+			sudo -u $app env "CONDA_DIR=$CONDA_DIR" bash "${CONDA_DIR}/Miniforge3-4.10.1-4-Linux-aarch64.sh" -bu -p "${CONDA_DIR}"
+			sudo -u $app env "PATH=$python_path" pip --cache-dir "$cache_dir" install -U torch==1.8.0 torchvision==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html 2>&1
 			sudo -u $app env "PATH=$python_path" conda install -y numpy psycopg2 cython pandas scikit-learn=0.24.1 scikit-image=0.18.1 spacy=2.3.5 gevent=20.12.1 matplotlib=3.3.2
 			pushd "$backend_path/faiss"
-				sudo -u $app cmake -B build .
-				sudo -u $app make -C build -j faiss
-				sudo -u $app make -C build -j swigfaiss
+				sudo -u $app env "PATH=$python_path" cmake -B build . -DFAISS_ENABLE_GPU=OFF -DFAISS_ENABLE_PYTHON=ON -DFAISS_OPT_LEVEL=generic
+				sudo -u $app env "PATH=$python_path" make -C build -j faiss
+				sudo -u $app env "PATH=$python_path" make -C build -j swigfaiss
 				cd "build/faiss/python"
 				sudo -u $app env "PATH=$python_path" python setup.py install
 			popd
@@ -74,7 +78,7 @@ function set_up_virtualenv {
 			sed -i "s/Pillow==8.1.0/Pillow>=8.1.2/" "$backend_path/requirements.txt"
 			sed -i "/faiss-cpu==1.7.0/d" "$backend_path/requirements.txt"
 		else
-			sudo -u $app env "PATH=$python_path" pip --cache-dir "$cache_dir" install -U torch==1.8.0+cpu torchvision==0.9.0+cpu -f https://download.pytorch.org/whl/torch_stable.html 2>&1 || sudo -u $app "$pip" --cache-dir "$cache_dir" install -U torch==1.8.0 torchvision==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html 2>&1
+			sudo -u $app env "PATH=$python_path" pip --cache-dir "$cache_dir" install -U torch==1.8.0+cpu torchvision==0.9.0+cpu -f https://download.pytorch.org/whl/torch_stable.html 2>&1
 		fi
 		pushd "$backend_path/dlib"
 			sudo -u $app env "PATH=$python_path" python setup.py install 2>&1
@@ -82,9 +86,9 @@ function set_up_virtualenv {
 		sudo -u $app env "PATH=$python_path" pip --cache-dir "$cache_dir" install -U --requirement "$backend_path/requirements.txt" 2>&1
 		sudo -u $app env "PATH=$python_path" pip --cache-dir "$cache_dir" install -U --requirement "$backend_path/requirements-ynh.txt" 2>&1
 		sudo -u $app env "PATH=$python_path" python -m spacy download en_core_web_sm 2>&1
-		if [ "$arch" = "aarch64" ]; then
-			unzip "$CONDA_DIR/lib/python3.8/site-packages/"faiss*.egg -d "$CONDA_DIR/lib/python3.8/site-packages/"
-		fi
+		#if [ "$arch" = "arm64" ] || [ "$arm64_test" -eq 1 ]; then
+			#sudo -u $app unzip "$CONDA_DIR/lib/python3.8/site-packages/"faiss*.egg -d "$CONDA_DIR/lib/python3.8/site-packages/"
+		#fi
 		chown -R root:root "$backend_path"
 	popd
 }
@@ -124,27 +128,22 @@ function add_configuations {
 	fi
 
 	ynh_add_config --template="librephotos.env" --destination="$final_path/librephotos.env"
+}
 
-	for file in $final_path/bin/*; do
-		ynh_replace_string -m '#!/usr/bin/env bash' -r "#!/usr/bin/env bash\nsource $final_path/librephotos.env" -f $file
-		echo "$(uniq $file)" > $file
-		ynh_replace_string -m "source $final_path/librephotos.env" -r "source $final_path/librephotos.env\nexport PATH=\$NODEJS_PATH:\$PATH" -f $file
-		echo "$(uniq $file)" > $file
-		ynh_replace_string -m "/usr/lib/librephotos" -r "$final_path" -f $file
-		ynh_replace_string -m 3000 -r '$httpPort' -f $file
-		ynh_replace_string -m 8001 -r '$BACKEND_PORT' -f $file
-		ynh_replace_string -m 8002 -r '$IMAGE_SIMILARITY_SERVER_PORT' -f $file
-		ynh_replace_string -m "/etc/librephotos" -r $final_path -f $file
-		ynh_replace_string -m "librephotos-backend.env" -r "librephotos.env" -f $file
-		ynh_replace_special_string -m 'su - -s $(which bash) librephotos << EOF' -r '' -f $file
-		ynh_replace_special_string -m 'EOF' -r '' -f $file
-		ynh_replace_string -m "python3" -r "$backend_path/venv/bin/python3" -f $file
-		ynh_replace_string -m "gunicorn --workers" -r "$backend_path/venv/bin/gunicorn --workers" -f $file
-		ynh_replace_string -m "$backend_path/venv/bin/$backend_path/venv/bin/python" -r "$backend_path/venv/bin/python" -f $file
-		ynh_replace_string -m "$backend_path/venv/bin/$backend_path/venv/bin/gunicorn --workers" -r "$backend_path/venv/bin/gunicorn --workers" -f $file
-		ynh_replace_string -m 'npm install' -r " " -f $file
-		ynh_replace_string -m 'npm' -r "yarn" -f $file
-	done
+function upgrade_db {
+	pushd "$final_path/backend"
+		chown -R $app:$app "$final_path/backend"
+		chown -R $app:$app "/var/log/$app"
+		sudo -u $app bash -c "
+			set -a
+			export PATH=\"$path_prefix:"'$PATH'"\"
+			source \"$final_path\"/librephotos.env
+			python3 manage.py showmigrations
+			python3 manage.py migrate 
+			python3 manage.py showmigrations
+		" 2>&1
+	popd
+	set_permissions
 }
 
 function set_permissions {
